44a334b587d1b2ac987da8b9daefed5ac8ae76b1
Sungho Cho <didog9595@gmail.com>
2022-05-09 16:53:02 -0600

    Find package in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="janggi",
     version="0.0.1",
     install_requires=["termcolor==1.1.0"],
     package_dir={"": "janggi"},
+    packages=find_packages(where="janggi"),
 )
