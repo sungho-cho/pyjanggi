074c44d390553a71cef7a38fb5dced279482ec49
Sungho Cho <didog9595@gmail.com>
2022-05-12 00:27:37 -0700

    Update package version to 0.9.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="janggi",
-    version="0.0.1",
+    version="0.9.0",
     install_requires=["termcolor==1.1.0"],
     package_dir={"": "janggi"},
     packages=find_packages(where="janggi"),
